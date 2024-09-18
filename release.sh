1becb804d4be5d6b58c21767a6eae6d7612b64ca
Andrew Dunn <boxheed@users.noreply.github.com>
2024-09-18 22:40:28 +0000

    feat: created a new release script

---
+++ release.sh
@@ -0,0 +1,42 @@
+#!/bin/env bash
+
+DEVELOP_BRANCH="develop"
+
+git remote -v
+
+# Determine branch name
+if git rev-parse --quiet --verify origin/main > /dev/null; then
+    MAIN_BRANCH="main"
+elif git rev-parse --quiet --verify origin/master > /dev/null; then
+    MAIN_BRANCH="master"
+else
+    echo "Error: Unable to determine the main branch. Exiting."
+    exit 1
+fi
+
+# Fetch changes from remote repo and determine difference
+git fetch origin
+git pull
+DEVELOP_AHEAD_COUNT=$(git rev-list --count origin/${DEVELOP_BRANCH} ^origin/${MAIN_BRANCH})
+if [ "$DEVELOP_AHEAD_COUNT" -gt 0 ]; then
+    # Check if all the commits on the develop branch are by 'dependabot'
+    echo "The develop branch is ahead of main branch. Opening PR"
+    PR_OUTPUT=$(gh pr create --base ${MAIN_BRANCH} --head ${DEVELOP_BRANCH} --title "Release: auto-generated PR" --body "This pull request is automatically generated." 2>&1)
+    PR_URL=$(echo "$PR_OUTPUT" | grep -oP 'https://github\.com/.+')
+
+    if [ -n "$PR_URL" ]; then
+        echo "Merging Pull Request: $PR_URL"
+        gh pr checks ${PR_URL} --watch
+        # Merge the pull request
+        gh pr merge --merge "${PR_URL}"
+    else
+        echo "Error: Unable to extract the pull request URL from the output."
+        exit 1
+    fi
+else
+    echo "No action needed. The develop branch is not ahead of the main branch."
+fi
+    
+
+
+
