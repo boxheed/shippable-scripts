98279d63acd1ea5a4135b3cb642f154dee0c2446
Andrew Dunn <boxheed@users.noreply.github.com>
2024-09-23 21:38:25 +0000

    feat: Adding script to update gradle version

---
+++ update-all-gradles.sh
@@ -0,0 +1,36 @@
+#!/bin/env bash
+
+
+gradle_version=$1
+# Loop through all subfolders
+for dir in */; do
+    # Change directory to the subfolder
+    cd "$dir"
+    
+    if [ -d "gradle" ]; then
+        
+        
+        echo "###########################################"
+        echo "Processing $dir"
+        echo "###########################################"
+        ./gradlew -version | grep "Gradle $gradle_version"
+        if [ $? -eq 0 ]; then
+            echo "Skipping $dir, Gradle already upto date."
+        else
+            echo "Upgrading Gradle to $gradle_version."
+            set -e
+            git remote -v
+            git pull
+            ./gradlew wrapper --gradle-version $gradle_version --distribution-type bin
+            ./gradlew build
+            git add -A
+            git commit -m "chore(ci): upgrade gradle to $gradle_version"
+            git push
+            set +e
+        fi
+        ./gradlew -version
+    fi
+    
+    # Change back to the parent directory
+    cd ..
+done
